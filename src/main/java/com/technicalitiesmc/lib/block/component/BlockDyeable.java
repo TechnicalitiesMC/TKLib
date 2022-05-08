7301e9052de5c6a1b62cef7c0357c03e97211345
amadornes <amadornes@gmail.com>
2022-05-08 16:20:00 +0200

    Add component for dyeable blocks

---
+++ src/main/java/com/technicalitiesmc/lib/block/component/BlockDyeable.java
@@ -0,0 +1,54 @@
+package com.technicalitiesmc.lib.block.component;
+
+import com.technicalitiesmc.lib.block.BlockComponent;
+import com.technicalitiesmc.lib.util.Utils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class BlockDyeable extends BlockComponent.WithoutData {
+
+    private final EnumProperty<DyeColor> property;
+
+    private BlockDyeable(
+            Context context,
+            EnumProperty<DyeColor> property
+    ) {
+        super(context);
+        this.property = property;
+    }
+
+    public static Constructor<BlockDyeable> of(EnumProperty<DyeColor> property) {
+        return ctx -> new BlockDyeable(ctx, property);
+    }
+
+    public DyeColor color(BlockState state) {
+        return state.getValue(property);
+    }
+
+    public BlockState withColor(BlockState state, DyeColor color) {
+        return state.setValue(property, color);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        var item = player.getItemInHand(hand);
+        if (!item.isEmpty()) {
+            var dyeColor = Utils.getDyeColor(item);
+            if (dyeColor != null) {
+                if (!level.isClientSide()) {
+                    level.setBlock(pos, state.setValue(property, dyeColor), 3);
+                }
+                return InteractionResult.sidedSuccess(level.isClientSide());
+            }
+        }
+        return super.use(state, level, pos, player, hand, hit);
+    }
+
+}
