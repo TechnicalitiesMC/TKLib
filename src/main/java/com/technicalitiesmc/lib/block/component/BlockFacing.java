19c0d630e59cad55eebfc335f9bcf5a3cd1b753a
amadornes <amadornes@gmail.com>
2022-05-24 00:46:22 +0200

    Add rotation support to BlockFacing component

--- src/main/java/com/technicalitiesmc/lib/block/component/BlockFacing.java
+++ src/main/java/com/technicalitiesmc/lib/block/component/BlockFacing.java
@@ -1,10 +1,17 @@
 package com.technicalitiesmc.lib.block.component;
 
 import com.technicalitiesmc.lib.block.BlockComponent;
+import com.technicalitiesmc.lib.util.Utils;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
+
+import java.util.function.UnaryOperator;
 
 public class BlockFacing extends BlockComponent.WithoutData {
 
@@ -43,6 +50,23 @@ public class BlockFacing extends BlockComponent.WithoutData {
     protected BlockState getStateForPlacement(BlockPlaceContext context, BlockState state) {
         var lookingDirection = context.getNearestLookingDirection();
         return state.setValue(property, towardsPlayer ? lookingDirection.getOpposite() : lookingDirection);
+    }
+
+    @Override
+    protected BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(property, rotation.rotate(state.getValue(property)));
+    }
+
+    protected BlockState rotate(BlockState state, Direction.Axis axis, Rotation rotation) {
+        return state.setValue(property, Utils.rotate(state.getValue(property), axis, rotation));
+    }
+
+    @Override
+    protected InteractionResultHolder<UnaryOperator<BlockState>> rotate(BlockState state, Level level, BlockPos pos, Direction.Axis axis, Rotation rotation) {
+        if (rotate(state, axis, rotation) == state) {
+            return InteractionResultHolder.pass(UnaryOperator.identity());
+        }
+        return InteractionResultHolder.success(s -> rotate(s, axis, rotation));
     }
 
 }
