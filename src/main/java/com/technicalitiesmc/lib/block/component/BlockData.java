a95698aa4c8e1b682f0d88b214d312c6d751e946
amadornes <amadornes@gmail.com>
2022-05-24 00:46:42 +0200

    Allow BlockData to optionally not be serialized

--- src/main/java/com/technicalitiesmc/lib/block/component/BlockData.java
+++ src/main/java/com/technicalitiesmc/lib/block/component/BlockData.java
@@ -10,16 +10,25 @@ import javax.annotation.Nullable;
 
 public class BlockData<T extends BlockComponentData> extends BlockComponent.WithData<T> {
 
-    public BlockData(Context context, BlockComponentData.Constructor<T> constructor) {
+    private final boolean serialized;
+
+    private BlockData(Context context, BlockComponentData.Constructor<T> constructor, boolean serialized) {
         super(context, constructor);
+        this.serialized = serialized;
     }
 
     // Static construction
 
     public static <T extends BlockComponentData> BlockComponent.Constructor<BlockData<T>> of(
+            BlockComponentData.Constructor<T> constructor, boolean serialized
+    ) {
+        return context -> new BlockData<>(context, constructor, serialized);
+    }
+
+    public static <T extends BlockComponentData> BlockComponent.Constructor<BlockData<T>> of(
             BlockComponentData.Constructor<T> constructor
     ) {
-        return context -> new BlockData<>(context, constructor);
+        return of(constructor, true);
     }
 
     // API
@@ -27,6 +36,14 @@ public class BlockData<T extends BlockComponentData> extends BlockComponent.With
     @Nullable
     public T at(BlockGetter level, BlockPos pos, BlockState state) {
         return getData(level, pos, state);
+    }
+
+    // Implementation
+
+
+    @Override
+    protected boolean isSerialized() {
+        return serialized;
     }
 
 }
